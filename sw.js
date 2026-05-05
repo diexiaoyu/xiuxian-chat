5e16f491c3bc3bc71d2ebf803c55aeb3794c65f7
diexiaoyu <3306662796@qq.com>
2026-05-05 23:41:11 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,16 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(clients.claim());
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
+  // 跳过对 manifest.json 的缓存
+  if (event.request.url.includes('manifest.json')) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
   event.respondWith(fetch(event.request));
 });
